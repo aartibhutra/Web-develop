0f9221517007dc847a31d1db07cdd2c11b902dad
aartibhutra <aartimaheshwari160@gmail.com>
2024-11-01 14:44:46 +0530

    change

--- Javascript/6.Loops/script.js
+++ Javascript/6.Loops/script.js
@@ -1,9 +1,24 @@
 // for loop
 // do while loop 
-// while loop 
+// while loop
+//note:  a for loop is usually used when the number of iterations is known and while and do while loop are usually used when number of iterations are unknown  
+
+// break and continue: the break statement is used terminate the loop immediately when it is encountered 
+// the continue statement is used to skip the current iteration of the loop and the control flow of the program goes to the next iteration 
 
 
-for(let index =0;index<=8;index++){
-    console.log('hello');
-    
+for(let index =0;index<=5;index++){
+    console.log('hello',index);
 }
+
+let step =0;
+while(step<5){
+    console.log(step,'step');
+    step++;
+} 
+
+let i =1;
+do{
+    console.log(i,'hii');
+    i++;
+}while(i<0);
---
+++ Javascript/7.Arrays/script.js
@@ -0,0 +1,20 @@
+// creating an array by using [] 
+// arrays in javascript can be mixed data types.
+
+// you can find the length of an element using the length property 
+// const length = newData.length;
+// elements stored in continuous form 
+
+const arr =["Aarti","Raunak"];
+console.log(arr);
+const neww = arr;
+neww[1]="bharti";
+console.log(neww);
+console.log(arr);
+
+const char= ['h','e','l','l','o'];
+console.log(char[0]);
+
+const array =["Aarti","Raunak",true,0,{name:"aaru"},function hello(){console.log("hello world");}];
+console.log(array[5]);
+console.log(array.length);
